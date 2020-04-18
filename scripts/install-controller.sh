99a3d174177a6602110ca750fb12569ac0fab30e
suyog.patil <suyog.patil@rakuten.com>
2020-04-18 17:02:37 +0800

    update shell

--- scripts/install-controller.sh
+++ scripts/install-controller.sh
@@ -23,8 +23,8 @@ sudo apt-get install -y kubectl
 # Install/prepare etcd
 wget -q --show-progress --https-only --timestamping \
   "https://github.com/etcd-io/etcd/releases/download/v3.4.0/etcd-v3.4.0-linux-amd64.tar.gz"
-tar -xvf etcd-v3.4.0-linux-amd64.tar.gz
-sudo mv etcd-v3.4.0-linux-amd64/etcd* /usr/local/bin/
+tar -xf etcd-v3.4.0-linux-amd64.tar.gz
+sudo mv -v etcd-v3.4.0-linux-amd64/etcd* /usr/local/bin/
 
 # Provision Kubernetes Control Plane
 sudo mkdir -p /etc/kubernetes/config
